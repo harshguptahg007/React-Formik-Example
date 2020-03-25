086eee93ef3f7d82e42a3dd031d131007a59edca
Harsh Gupta <harshguptahg007@gmail.com>
2020-03-25 16:40:20 +0530

    Updates

---
+++ precache-manifest.f0ca50a1fe85859280743428c45b8e1c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2fd5998a23e71bc892f970628a49072",
+    "url": "/React-Formik-Example/index.html"
+  },
+  {
+    "revision": "6997e9e3845cf5e5ce30",
+    "url": "/React-Formik-Example/static/css/main.72173c73.chunk.css"
+  },
+  {
+    "revision": "c6cfb4db87fbf7abf25b",
+    "url": "/React-Formik-Example/static/js/2.ccc0efbc.chunk.js"
+  },
+  {
+    "revision": "1f83015dc635997a697ee225e2f4c347",
+    "url": "/React-Formik-Example/static/js/2.ccc0efbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6997e9e3845cf5e5ce30",
+    "url": "/React-Formik-Example/static/js/main.1c0119ed.chunk.js"
+  },
+  {
+    "revision": "17a960d6ee2623856eda",
+    "url": "/React-Formik-Example/static/js/runtime-main.3f9766b4.js"
+  },
+  {
+    "revision": "5f63ec1d8eabb94e41550efbc2bdcc0b",
+    "url": "/React-Formik-Example/static/media/survey-form-background.5f63ec1d.jpeg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/React-Formik-Example/precache-manifest.f0ca50a1fe85859280743428c45b8e1c.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Formik-Example/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
